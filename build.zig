d26db39133f296d5aaeddbd3fece1852c52d57e5
Qiming Chu <cchuqiming@gmail.com>
2024-12-26 18:46:28 +0800

    build.zig: bump NEMU to master branch
    NEMU use capstone as a disassembler now

--- build.zig
+++ build.zig
@@ -4,118 +4,46 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     const optimize = b.standardOptimizeOption(.{});
+    const riscv32 = b.option(bool, "is_rv32", "Choose riscv32") orelse false;
 
-    // build riscv32 library
-    const riscv32 = b.addStaticLibrary(.{
-        .name = "riscv32",
-        .target = target,
-        .optimize = optimize,
-    });
-    riscv32.addCSourceFiles(.{ .root = b.path("src/isa/riscv32"), .files = &.{
-        "reg.c",
-        "init.c",
-        "logo.c",
-        "reg.c",
-        "inst.c",
-        "difftest/dut.c",
-        "system/mmu.c",
-        "system/intr.c",
-    } });
-    riscv32.linkLibC();
-    riscv32.addIncludePath(b.path("include"));
-    riscv32.addIncludePath(b.path("src/isa/riscv32/include"));
-    riscv32.addIncludePath(b.path("src/isa/riscv32/local-include"));
-    riscv32.defineCMacro("__GUEST_ISA__", "riscv32");
-
-    // TODO: llvm-18 and SDL2 still cannot be linked to nemu on macOS
-    const dummy = b.addStaticLibrary(.{
-        .name = "dummy",
-        .target = target,
-        .optimize = optimize,
-    });
-
-    dummy.addCSourceFiles(.{
-        .files = &.{
-            "dummy.c",
-        },
-        .flags = &.{
-            "-Wall",
-            "-W",
-            "-fPIE",
-            "-std=c++17",
-        },
-    });
-    dummy.linkLibC();
-
-    const device = b.addStaticLibrary(.{
-        .name = "device",
-        .target = target,
-        .optimize = optimize,
-    });
-    device.addCSourceFiles(.{
-        .root = b.path("src/device"),
-        .files = &.{
-            "dummy.c",
-            "io/map.c",
-            "io/mmio.c",
-            "io/port-io.c",
-            "alarm.c",
-            "audio.c",
-            "disk.c",
-            "intr.c",
-            "keyboard.c",
-            "serial.c",
-            "timer.c",
-            "vga.c",
-        },
-    });
-    device.linkLibC();
-    device.addIncludePath(b.path("include"));
-    device.addIncludePath(b.path("src/isa/riscv32/include"));
-    device.addIncludePath(b.path("src/isa/riscv32/local-include"));
-    device.defineCMacro("__GUEST_ISA__", "riscv32");
-    device.linkSystemLibrary("SDL2"); // FIXME: SDL2 still cannot be linked to nemu on macOS
-    device.linkSystemLibrary("readline");
+    const cflags = .{ "-pie", "-g" };
 
     const nemu = b.addExecutable(.{
         .name = "nemu",
         .target = target,
         .optimize = optimize,
     });
-    nemu.defineCMacro("__GUEST_ISA__", "riscv32"); // set guest ISA
     nemu.defineCMacro("ITRACE_COND", "false"); // disable instruction trace
     nemu.linkLibC();
     nemu.linkSystemLibrary("dl"); // for dlopen
-    nemu.linkSystemLibrary("SDL2"); // for device
     nemu.linkSystemLibrary("readline"); // for readline
     nemu.addIncludePath(b.path("include"));
     nemu.addIncludePath(b.path("src/monitor/sdb"));
-    nemu.addIncludePath(b.path("src/isa/riscv32/include"));
-    nemu.addIncludePath(b.path("src/isa/riscv32/local-include"));
+    nemu.addIncludePath(b.path("tools/capstone/repo/include"));
+    nemu.addLibraryPath(b.path("tools/capstone/repo"));
     nemu.addCSourceFiles(.{
         .root = b.path("src"),
-        .files = &.{
-            "nemu-main.c",
-            "cpu/cpu-exec.c",
-            "cpu/difftest/dut.c",
-            "engine/interpreter/init.c",
-            "engine/interpreter/hostcall.c",
-            "memory/paddr.c",
-            "memory/vaddr.c",
-            "monitor/monitor.c",
-            "monitor/sdb/expr.c",
-            "monitor/sdb/sdb.c",
-            "monitor/sdb/watchpoint.c",
-            "utils/log.c",
-            "utils/timer.c",
-            "utils/state.c",
-        },
-        .flags = &.{"-pie"},
+        .files = &.{ "nemu-main.c", "cpu/cpu-exec.c", "cpu/difftest/dut.c", "engine/interpreter/init.c", "engine/interpreter/hostcall.c", "memory/paddr.c", "memory/vaddr.c", "monitor/monitor.c", "monitor/sdb/expr.c", "monitor/sdb/sdb.c", "monitor/sdb/watchpoint.c", "utils/log.c", "utils/timer.c", "utils/state.c", "utils/disasm.c" },
+        .flags = &cflags,
     });
-    nemu.linkLibrary(riscv32);
-    nemu.linkLibrary(device);
-    nemu.linkLibrary(dummy);
-    // FIXME: nemu cannot fetch inst from pmem
-
+    if (riscv32) {
+        nemu.defineCMacro("__GUEST_ISA__", "riscv32"); // set guest ISA
+        nemu.addCSourceFiles(.{
+            .root = b.path("src/isa/riscv32"),
+            .files = &.{
+                "reg.c",
+                "init.c",
+                "logo.c",
+                "inst.c",
+                "difftest/dut.c",
+                "system/mmu.c",
+                "system/intr.c",
+            },
+            .flags = &cflags,
+        });
+        nemu.addIncludePath(b.path("src/isa/riscv32/include"));
+        nemu.addIncludePath(b.path("src/isa/riscv32/local-include"));
+    }
     b.installArtifact(nemu);
 }
+
